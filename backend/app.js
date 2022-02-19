4abd3c62779c17053f822ee12a8dd2c78a7bc961
Dmitrie <dmitriebivol@gmail.com>
2022-02-19 21:21:14 +0000

    changed server

--- backend/app.js
+++ backend/app.js
@@ -14,8 +14,8 @@ const API_KEY="AIzaSyCBkw_uhBVIOIqek8TbGZXex8pszTc8Qnk";
 app.use(express.json());
 app.use(express.urlencoded());
 
-async function GetNearbyPlaces(type,lat,long,radius){
-    let url= `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat}%2C${long}&radius=${radius}&type=${type}&key=${API_KEY}`
+async function GetNearbyPlaces(type,lat,lng,radius){
+    let url= `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat}%2C${lng}&radius=${radius}&type=${type}&key=${API_KEY}`
     let google_resp=await fetch(url);
     let body=await google_resp.json();
     return await body
@@ -25,10 +25,24 @@ async function GetNearbyPlaces(type,lat,long,radius){
 app.get('/nearbyPlaces', async(req,res)=>{
     let type=req.query.type;
     let lat=req.query.lat;
-    let long=req.query.long;
+    let lng=req.query.lng;
     let radius=1500;
-    let body=await GetNearbyPlaces(type,lat,long,radius);
-    res.send(body);
+    let body=await GetNearbyPlaces(type,lat,lng,radius);
+    let response=[];
+    for (let result of body.results){
+        if (result.business_status==="OPERATIONAL"){
+            response.push({
+                name:result.name,
+                lat:result.geometry.location.lat,
+                lng:result.geometry.location.lng,
+                rating:result.rating,
+                user_ratings_total:result.user_ratings_total
+            })
+        }
+    }
+    console.log(response)
+    res.send(response);
+    
 })
 
 async function GetTravelTime(originList,destinationsList,mode){
@@ -46,10 +60,12 @@ async function GetTravelTime(originList,destinationsList,mode){
     }
     let formattedDestinations=tempList.join("%7C");
 
-    let url= `'https://maps.googleapis.com/maps/api/distancematrix/json?origins=${formattedOrigins}&destinations=&key=${API_KEY}&mode=${mode}`;
+    let url= `'https://maps.googleapis.com/maps/api/distancematrix/json?origins=${formattedOrigins}&destinations=${formattedDestinations}&key=${API_KEY}&mode=${mode}`;
     let google_resp=await fetch(url);
     let body=await google_resp.json();
+    return await body
 }
+
 
 
 
--- src/App.jsx
+++ src/App.jsx
@@ -13,18 +13,6 @@ const base_style = {
   fontFamily: "Arial",
 };
 
-function GetCentroid(userCoordinates){
-
-  let sumLat=0;
-  let sumLong=0;
-  for (let coordinate in userCoordinates){
-    sumLat+=coordinate[0];
-    sumLong+=coordinate[1];
-  }
-  let avgLat=sumLat/(userCoordinates.length);
-  let avgLong=sumLong/(userCoordinates.length);
-  return (avgLat,avgLong);
-}
 
 let onMapClick = event => {
   let { lat, lng } = event;
