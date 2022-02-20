41f614c0029b34681ca2788e0afd157eb2578b3d
Dmitrie <dmitriebivol@gmail.com>
2022-02-20 06:50:41 +0000

    addded different colour markers png. Made server return more properties of results

--- backend/app.js
+++ backend/app.js
@@ -13,6 +13,9 @@ const API_KEY="AIzaSyCBkw_uhBVIOIqek8TbGZXex8pszTc8Qnk";
 app.use(express.json());
 app.use(express.urlencoded());
 
+const SEARCH_RADIUS=1000;   //radius of search area around the centroid
+const HOW_MANY=5;   //how many results to aggregate
+
 async function GetNearbyPlaces(type,lat,lng,radius){
     let url= `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat}%2C${lng}&radius=${radius}&type=${type}&key=${API_KEY}`
     let google_resp=await fetch(url);
@@ -22,7 +25,7 @@ async function GetNearbyPlaces(type,lat,lng,radius){
 }
 
 
-function filterPlaces(body,howMany=5) {  //exclude unnecessary attributes from the json response
+function filterPlaces(body,howMany) {  //exclude unnecessary attributes from the json response
     let response=[];
     let count=0
     for (let result of body.results){
@@ -43,8 +46,8 @@ function filterPlaces(body,howMany=5) {  //exclude unnecessary attributes from t
     return response;
 }
 
-let originsList=[[54.7783543,-1.577],[54.7771651,-1.5547943],[54.7736351,-1.5919927]]//[TA,DB]
-let destinationsList=[[54.7745788915615, -1.5756803066541583],[54.77523793773805, -1.575823132909281]]
+let originsList=[[54.7783543,-1.577],[54.7771651,-1.5547943],[54.7736351,-1.5919927]]//[TA,DB]   //mock
+let destinationsList=[[54.7745788915615, -1.5756803066541583],[54.77523793773805, -1.575823132909281]]   //mock
 
 
 async function GetTravelTimes(originsList,destinationsList,mode){
@@ -71,12 +74,12 @@ async function GetTravelTimes(originsList,destinationsList,mode){
 
 async function GetDistancesToPlaces(type,lat,lng,originList,mode){
 
-    let radius=1000;
-    let body=await GetNearbyPlaces(type,lat,lng,radius);
+  
+    let body=await GetNearbyPlaces(type,lat,lng,SEARCH_RADIUS);
     if (body.length===0){
         return null
     }
-    let jsonPlaces=filterPlaces(body,5);
+    let jsonPlaces=filterPlaces(body,HOW_MANY);
 
     let placesCoordinates=[];
     for (let jsonPlace of jsonPlaces){
@@ -102,29 +105,32 @@ async function GetDistancesToPlaces(type,lat,lng,originList,mode){
 
 }
 
-app.get('/getDistancesToPlaces', async(req,res)=>{
-    let type=req.query.type;
-    let lat=req.query.lat;
-    let lng=req.query.lng;
-    let originsListFormatted=req.query.originsList;
-    let mode=req.query.mode;
-    let originsList=[];
-    for (let origin of originsListFormatted.split("|")){
-        let lat=parseFloat(origin.split(",")[0])
-        let lng=parseFloat(origin.split(",")[1])
-        originsList.push([lat,lng])
-    }
-    let body=await GetDistancesToPlaces(type,lat,lng,originsList,mode);
-    res.send(body);
+// app.get('/getDistancesToPlaces', async(req,res)=>{
+//     let type=req.query.type;
+//     let lat=req.query.lat;
+//     let lng=req.query.lng;
+//     let originsListFormatted=req.query.originsList;
+//     let mode=req.query.mode;
+//     let originsList=[];
+//     for (let origin of originsListFormatted.split("|")){
+//         let lat=parseFloat(origin.split(",")[0])
+//         let lng=parseFloat(origin.split(",")[1])
+//         originsList.push([lat,lng])
+//     }
+//     let body=await GetDistancesToPlaces(type,lat,lng,originsList,mode);
+//     res.send(body);
     
-})
+// })
 
 async function GetOptimal(type,centroidLat,centroidLng,mode){
     
-    let body=await GetDistancesToPlaces(type,centroidLat,centroidLng,originsList,mode);
+    let body=await GetNearbyPlaces(type,centroidLat,centroidLng,SEARCH_RADIUS);
     if (!body){
         return null
     }
+    let jsonPlaces=filterPlaces(body,HOW_MANY);
+
+    body=await GetDistancesToPlaces(type,centroidLat,centroidLng,originsList,mode);
     let sumDistance=[]
     let sumDuration=[]
     for (let origin of body[0]){
@@ -144,22 +150,30 @@ async function GetOptimal(type,centroidLat,centroidLng,mode){
         }
     }
 
-    return [sumDistance.sort((a,b) =>  a.sum-b.sum),sumDuration.sort((a,b) =>  a.sum-b.sum)]
-
-}
-
-function GetCentroid(userCoordinates){
-
-    let sumLat=0;
-    let sumLong=0;
-    for (let coordinate in userCoordinates){
-      sumLat+=coordinate[0];
-      sumLong+=coordinate[1];
+    let sortedByDistance=sumDistance.sort((a,b) =>  a.sum-b.sum);
+    let sortedByDuration=sumDuration.sort((a,b) =>  a.sum-b.sum);
+    
+    for (let result of sortedByDistance){
+        delete result.sum;
+        let temp=jsonPlaces.find((object)=>(object.name===result.name))
+        result.rating=temp.rating;
+        result.user_ratings_total=temp.user_ratings_total
+        result.lat=temp.lat;
+        result.lng=temp.lng
     }
-    let avgLat=sumLat/(userCoordinates.length);
-    let avgLong=sumLong/(userCoordinates.length);
-    return (avgLat,avgLong);
+    for (let result of sortedByDuration){
+        delete result.sum;
+        let temp=jsonPlaces.find((object)=>(object.name===result.name))
+        result.rating=temp.rating;
+        result.user_ratings_total=temp.user_ratings_total
+        result.lat=temp.lat;
+        result.lng=temp.lng
+    }
+
+    return [sortedByDistance,sortedByDuration]
+
 }
+
 
 app.get('/getOptimalPlaces', async(req,res)=>{
     let type=req.query.type;
@@ -173,9 +187,7 @@ app.get('/getOptimalPlaces', async(req,res)=>{
         let lng=parseFloat(origin.split(",")[1])
         originsList.push([lat,lng])
     }
-
-    let centroid=GetCentroid(originsList);
-    let body=await GetOptimal(type,centroid[0],centroid[1],originsList,mode);
+    let body=await GetOptimal(type,lat,lng,originsList,mode);
     if (!body){
         res.send("No results")
     }
@@ -184,5 +196,5 @@ app.get('/getOptimalPlaces', async(req,res)=>{
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`App listening on http://localhost:${port}`)
   })
