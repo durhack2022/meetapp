fcb2bef7ca93eb85f92c08e6dc69d5bf59dcb177
Dmitrie <dmitriebivol@gmail.com>
2022-02-20 04:51:15 +0000

    Working server maybe

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,6 @@ const port = 3080;
 
 var cors = require('cors');
 
-//sam and jenni, this is something I have included to allow access to the API when hosted on a different domain to the front end
 app.use(cors());
 
 const path = require("path");
@@ -18,56 +17,157 @@ async function GetNearbyPlaces(type,lat,lng,radius){
     let url= `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat}%2C${lng}&radius=${radius}&type=${type}&key=${API_KEY}`
     let google_resp=await fetch(url);
     let body=await google_resp.json();
+
     return await body
 }
 
 
-app.get('/nearbyPlaces', async(req,res)=>{
-    let type=req.query.type;
-    let lat=req.query.lat;
-    let lng=req.query.lng;
-    let radius=1500;
-    let body=await GetNearbyPlaces(type,lat,lng,radius);
+function filterPlaces(body,howMany=5) {  //exclude unnecessary attributes from the json response
     let response=[];
+    let count=0
     for (let result of body.results){
-        if (result.business_status==="OPERATIONAL"){
-            response.push({
-                name:result.name,
-                lat:result.geometry.location.lat,
-                lng:result.geometry.location.lng,
-                rating:result.rating,
-                user_ratings_total:result.user_ratings_total
-            })
-        }
+            if (result.business_status==="OPERATIONAL"){
+                count+=1;
+                response.push({
+                    name:result.name,
+                    lat:result.geometry.location.lat,
+                    lng:result.geometry.location.lng,
+                    rating:result.rating,
+                    user_ratings_total:result.user_ratings_total
+                })
+            }
+            if (count===howMany){
+                break;
+            }
     }
-    console.log(response)
-    res.send(response);
-    
-})
+    return response;
+}
 
-async function GetTravelTime(originList,destinationsList,mode){
+let originsList=[[54.7783543,-1.577],[54.7771651,-1.5547943],[54.7736351,-1.5919927]]//[TA,DB]
+let destinationsList=[[54.7745788915615, -1.5756803066541583],[54.77523793773805, -1.575823132909281]]
+
+
+async function GetTravelTimes(originsList,destinationsList,mode){
+
     let tempList=[];
-    for (let origin in originList){
+    for (let origin of originsList){
         let temp = origin.join("%2C");
         tempList.push(temp);
     }
     let formattedOrigins=tempList.join("%7C");
 
     tempList=[];
-    for (let destination in destinationsList){
+    for (let destination of destinationsList){
         let temp = destination.join("%2C");
         tempList.push(temp);
     }
     let formattedDestinations=tempList.join("%7C");
 
-    let url= `'https://maps.googleapis.com/maps/api/distancematrix/json?origins=${formattedOrigins}&destinations=${formattedDestinations}&key=${API_KEY}&mode=${mode}`;
+    let url= `https://maps.googleapis.com/maps/api/distancematrix/json?origins=${formattedOrigins}&destinations=${formattedDestinations}&key=${API_KEY}&mode=${mode}`;
     let google_resp=await fetch(url);
     let body=await google_resp.json();
-    return await body
+    return body.rows
+}
+
+async function GetDistancesToPlaces(type,lat,lng,originList,mode){
+
+    let radius=1000;
+    let body=await GetNearbyPlaces(type,lat,lng,radius);
+    let jsonPlaces=filterPlaces(body,5);
+
+    let placesCoordinates=[];
+    for (let jsonPlace of jsonPlaces){
+        placesCoordinates.push([parseFloat(jsonPlace.lat),parseFloat(jsonPlace.lng)]);
+    }
+    let lst = await GetTravelTimes(originList,placesCoordinates,mode)
+    
+    let results=[]
+    let count=0
+    for (let origin of lst){
+        let result=[]
+        for (let place of origin.elements){
+            result.push({
+                name:jsonPlaces[count].name,
+                distance:place.distance.value,
+                duration:place.duration.value
+            })
+            count=(count+1)%jsonPlaces.length
+        }
+        results.push(result)
+    }
+    return results;
+
+}
+
+app.get('/getDistancesToPlaces', async(req,res)=>{
+    let type=req.query.type;
+    let lat=req.query.lat;
+    let lng=req.query.lng;
+    let originsListFormatted=req.query.originsList;
+    let mode=req.query.mode;
+    let originsList=[];
+    for (let origin of originsListFormatted.split("|")){
+        let lat=parseFloat(origin.split(",")[0])
+        let lng=parseFloat(origin.split(",")[1])
+        originsList.push([lat,lng])
+    }
+    let body=await GetDistancesToPlaces(type,lat,lng,originsList,mode);
+    res.send(body);
+    
+})
+
+async function GetOptimal(type,centroidLat,centroidLng,mode){
+    
+    let body=await GetDistancesToPlaces(type,centroidLat,centroidLng,originsList,mode);
+
+    let sumDistance=[]
+    let sumDuration=[]
+    for (let origin of body[0]){
+        sumDistance.push({
+            name:origin.name,
+            sum:0
+        })
+        sumDuration.push({
+            name:origin.name,
+            sum:0
+        })
+    }
+    for (let origin of body ){
+        for (let i=0;i<body[0].length;i++){
+            sumDistance[i].sum+=parseInt(origin[i].distance)
+            sumDuration[i].sum+=parseInt(origin[i].duration)
+        }
+    }
+    // console.log(sumDistance)
+    // console.log(sumDuration)
+    // // let indexOfSmallestDistance=sumDistance.indexOf(Math.min.apply(Math, sumDistance))
+    // // let indexOfSmallestDuration=sumDuration.indexOf(Math.min.apply(Math, sumDuration))
+    
+    // console.log(sumDistance.sort((a,b) =>  a.sum-b.sum))
+    // console.log(sumDuration.sort((a,b) =>  a.sum-b.sum))
+
+    return [sumDistance.sort((a,b) =>  a.sum-b.sum),sumDuration.sort((a,b) =>  a.sum-b.sum)]
+
 }
 
 
-
+app.get('/getOptimalPlaces', async(req,res)=>{
+    let type=req.query.type;
+    let lat=req.query.lat;
+    let lng=req.query.lng;
+    let originsListFormatted=req.query.originsList;
+    let mode=req.query.mode;
+    let originsList=[];
+    for (let origin of originsListFormatted.split("|")){
+        let lat=parseFloat(origin.split(",")[0])
+        let lng=parseFloat(origin.split(",")[1])
+        originsList.push([lat,lng])
+    }
+    
+    let body=await GetOptimal(type,lat,lng,originsList,mode);
+    res.send(body);
+    
+})
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
